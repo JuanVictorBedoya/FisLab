57b6c1c590f531ff057151d44f85654813ce2475
Juan Carlos Labrandero <charly@localhost.localdomain>
2017-08-30 01:05:50 -0400

    TextInput (ui component) base code

---
+++ src/client/components/text-input.jsx
@@ -0,0 +1,26 @@
+
+/****************************************************************************************
+
+	Copyright (c) 2017, Juan Carlos Labrandero.
+	For conditions of distribution and use, see copyright notice in LICENSE
+
+****************************************************************************************/
+
+import React from 'react';
+
+/****************************************************************************************/
+
+class TextInput extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+		<input type="text">
+
+		</input>)
+	}
+}
+
+export { TextInput }
