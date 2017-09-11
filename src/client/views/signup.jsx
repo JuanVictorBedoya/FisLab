b1e8941e1f6f88f7bc3d3dff79a5730fb3c639b2
Juan Carlos Labrandero <charly@localhost.localdomain>
2017-09-11 19:32:08 -0400

    Signup initial  code

---
+++ src/client/views/signup.jsx
@@ -0,0 +1,89 @@
+
+/****************************************************************************************
+
+	Copyright (c) 2017, Juan Carlos Labrandero.
+	For conditions of distribution and use, see copyright notice in LICENSE
+
+****************************************************************************************/
+
+import React from 'react';
+
+/****************************************************************************************/
+
+class Signup extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<header>
+					
+				</header>
+				<main>
+					<div>
+						<div>
+							<div>
+								<svg version="1.1" viewBox="0 0 210 297" height="297mm" width="210mm">
+									<g style="display:inline" id="layer2">
+										<g style="stroke:url(#linearGradient989)" id="symbol">
+											<g id="g932" style="stroke:url(#linearGradient989);stroke-opacity:1">
+												<path id="path34"
+													d="M 50.646033,109.12494 50.144587,51.625819 104.13359,22.876257 157.454,52.127266 l 0.16715,56.997684 -53.65471,29.08386 z"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path id="path36"
+													d="M 50.646033,109.12494 77.55696,94.917312 130.87737,123.49973"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path id="path38"
+													d="m 77.55696,123.33258 53.32041,-28.415268 26.74378,14.207638"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path id="path40"
+													d="M 77.55696,94.917312 77.055513,36.916741"
+													style="fill:url(#radialGradient921);stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;fill-opacity:1" />
+												<path
+													id="path42"
+													d="M 50.311735,79.873937 103.96644,51.960118"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path
+													id="path44"
+													d="M 157.62115,80.208235 103.96644,51.960118"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path
+													id="path50"
+													d="M 130.87737,94.917312 V 37.418187"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path
+													id="path52"
+													d="m 103.96644,51.960118 0.16715,-29.083861"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+												<path
+													id="path54"
+													d="M 104.13359,108.9578 104.30074,79.706789 77.222664,65.666305"
+													style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+											</g>
+											<path
+												style="fill:none;stroke:url(#linearGradient989);stroke-width:1.5;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+												d="M 104.30074,79.706789 131.04452,66.167751"
+												id="path56" />
+										</g>
+									</g>
+								</svg>
+							</div>
+							<div>
+								<h4>Welcome</h4>
+								<p>Welcome to FisLab, our objetive is to enthuse students to conduct experiments by arousing their curiosity. This would help them in learning basic and advanced concepts through remote experimentation</p>
+							</div>
+						</div>
+
+						<div>
+
+						</div>
+					</div>
+				</main>
+			</div>
+		);
+	}
+}
+
+export { Signup };
