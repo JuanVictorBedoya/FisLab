2500bf4b0db0d8455c68649bf7da03faa174290f
Juan Carlos Labrandero <charly@localhost.localdomain>
2017-09-14 22:26:54 -0400

    Register page text correction

--- src/client/views/signup.jsx
+++ src/client/views/signup.jsx
@@ -7,15 +7,21 @@
 ****************************************************************************************/
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import {TextInput} from '../components/text-input.jsx';
 import {Button} from '../components/button.jsx';
+import {Form} from '../components/form.jsx';
+import {TextInput} from '../components/text-input.jsx';
 
 /****************************************************************************************/
 
 class Signup extends React.Component {
 	constructor(props) {
 		super(props);
+	}
+
+	onFormSubmit() {
+
 	}
 
 	render() {
@@ -74,32 +80,37 @@ class Signup extends React.Component {
 								</svg>
 							</div>
 							<div className="container">
-								<h4>Welcome</h4>
+								<h2>Bienvenido</h2>
 								<p>Welcome to FisLab, our objetive is to enthuse students to conduct experiments by arousing their curiosity. This would help them in learning basic and advanced concepts through remote experimentation</p>
 							</div>
 
-							<div className="container" style={{marginTop: '8rem'}}>
+							<div className="container" style={{marginTop: '6rem'}}>
 								<img src="/images/usfx-logo.png" alt="" style={{width: '5rem'}}/>
 								<img src="/images/facebook-logo.png" alt="" style={{width: '5rem'}}/>
-								<p>Welcome to FisLab, our objetive is to enthuse students to conduct experiments by arousing their curiosity. This would help them in learning basic and advanced concepts through remote experimentation</p>
+								<p className="note">También puedes <Link to="login">Iniciar Sesión</Link> con tu cuenta de facebook, o con tus datos de estudiante si perteneces a la Universidad Sanfrancisco Xavier de Chuquisaca.</p>
 							</div>
 						</div>
 
 						<div className="col s12 l6">
-							<div className="container">
-								<div>
-									<h2>Datos de registro</h2>
+							<Form onSubmit={this.onFormSubmit.bind(this)}>
+								<div className="container">
+									<div>
+										<h2>Datos de registro</h2>
+									</div>
+									<div>
+										<TextInput name="fname" label="Nombre" placeholder="Nombre" required={true}/>
+										<TextInput name="lname" label="Apellidos" placeholder="Apellidos" required={true}/>
+										<TextInput name="email" label="E-mail" placeholder="Tu email" type="email" required={true}/>
+										<TextInput name="company" label="Organizacion" placeholder="Organizacion" required={true}/>
+									</div>
+									<div style={{marginTop: '2rem'}}>
+										<Button text="Registrarme" type="submit"/>
+									</div>
+									<div>
+										<p className="note">Al hacer clic en "Registrarme", muestras conformidad con nuestras <Link to="/condiciones">Condiciones</Link> y aceptas haber leído nuestra <Link to="/politica-datos">Polica de datos</Link>.</p>
+									</div>
 								</div>
-								<div>
-									<TextInput name="fname" label="Nombre" placeholder="Nombre"/>
-									<TextInput name="lname" label="Apellidos" placeholder="Apellidos"/>
-									<TextInput name="company" label="Organizacion" placeholder="Organizacion"/>
-									<TextInput name="job" label="Ocupacion" placeholder="Ocupacion"/>
-								</div>
-								<div style={{marginTop: '2rem'}}>
-									<Button text="Registrarme"/>
-								</div>
-							</div>
+							</Form>
 						</div>
 					</div>
 				</main>
