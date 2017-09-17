be8c1841fbcafbfd0d050f8526a42c0548110daa
Juan Carlos Labrandero <charly@localhost.localdomain>
2017-09-17 17:49:30 -0400

    USFX sign-in front-end with transition

--- src/client/views/signin.jsx
+++ src/client/views/signin.jsx
@@ -21,14 +21,57 @@ import SignInStyle from '../styles/app-signin.scss';
 class SignIn extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.status = {
+			toMain: false,
+			pageClass: 'signin-page',
+			currentPageClass: 'signin-page signin-page-current'
+		};
 	}
 
-	onFormSubmit() {
+	componentDidMount() {
+		this.refs.signinMain.addEventListener('animationend', this.onTogglePagesAnimationEnd.bind(this), false);
+	}
 
+	onMainFormSubmit() {
+
+	}
+
+	onUsfxFormSubmit() {
+
+	}
+
+	onUsfxSignIn() {
+		this.togglePages(this.refs.signinUsfx, this.refs.signinMain);
+		this.status.toMain = false;
+	}
+
+	onBackToSignIn() {
+		this.togglePages(this.refs.signinMain, this.refs.signinUsfx);
+		this.status.toMain = true;
 	}
 
 	onSignInWithFacebook(facebookUser) {
-		console.log(facebookUser);
+
+	}
+	
+	onTogglePagesAnimationEnd() {
+		let mainp = this.refs.signinMain,
+			usfxp = this.refs.signinUsfx;
+
+		if(this.status.toMain) {
+			mainp.className = this.status.currentPageClass;
+			usfxp.className = this.status.pageClass;
+		}else {
+			mainp.className = this.status.pageClass;
+			usfxp.className = this.status.currentPageClass;
+		}
+	}
+
+	togglePages(inPage, outPage) {
+		outPage.classList.add('signin-page-moveToLeft');
+		inPage.classList.add('signin-page-moveFromRight');
+		inPage.classList.add('signin-page-on-transition');
 	}
 
 	render() {
@@ -38,36 +81,79 @@ class SignIn extends React.Component {
 					
 				</header>
 				<main>
-					<div className="signin">
-						<img src="/images/fislab-logo-128x128.png" alt="" style={{margin: '0 auto', display: 'block'}}/>
-
-						<Form onSubmit={this.onFormSubmit.bind(this)}>
-							<div>
-								<h2 style={{textAlign: 'center'}}>Iniciar sesión</h2>
+					<div ref="signinMain" className="signin-page signin-page-current">
+						<div className="signin-container">
+							<div className="row" style={{paddingTop: '1rem'}}>
+								<div className="col s12">
+									<img src="/images/fislab-logo-128x128.png" alt="" style={{margin: '0 auto', display: 'block'}}/>
+								</div>
 							</div>
-							<div>
-								<TextInput name="email" label="E-mail" placeholder="Tu email" type="email" required={true}/>
-								<TextInput name="passw" label="Contraseña" placeholder="Contraseña" type="password" required={true}/>
+							<div className="row">
+								<div className="col s12">
+									<Form onSubmit={this.onMainFormSubmit.bind(this)}>
+										<div>
+											<h3 style={{textAlign: 'center'}}>Iniciar sesión</h3>
+										</div>
+										<div>
+											<TextInput name="email" label="E-mail" placeholder="Tu email" type="email" required={true}/>
+											<TextInput name="passw" label="Contraseña" placeholder="Contraseña" type="password" required={true}/>
+										</div>
+										<div style={{marginTop: '1rem'}}>
+											<Button text="Iniciar sesión" type="submit"/>
+										</div>
+										<div>
+											<p className="note">Si aún no tienes una cuenta, puedes <Link to="/registro">Registrarte aquí</Link>. También puedes iniciar sesión usando uno de los siguientes servicios</p>
+										</div>
+										<div>
+											<button className="kep-login-usfx" type="button" onClick={this.onUsfxSignIn.bind(this)}>
+												<span>Acceso universitario</span>
+											</button>
+											<FacebookLogin
+												appId="136635746918254"
+												fields="name,email,picture"
+												textButton="Iniciar con facebook"
+												icon="fa-facebook"
+												callback={this.onSignInWithFacebook.bind(this)} />
+										</div>
+									</Form>
+								</div>
 							</div>
-							<div style={{marginTop: '1rem'}}>
-								<Button text="Iniciar sesión" type="submit"/>
+						</div>
+					</div>
+					<div ref="signinUsfx" className="signin-page">
+						<div className="signin-container">
+							<div className="row" style={{paddingTop: '1rem'}}>
+								<div className="col s12">
+									<img src="/images/fislab-logo-128x128.png" alt="" style={{margin: '0 auto', display: 'block'}}/>
+								</div>
 							</div>
-							<div>
-								<p className="note">Al hacer clic en "Registrarme", muestras conformidad con nuestras <Link to="/condiciones">Condiciones</Link> y aceptas haber leído nuestra <Link to="/politica-datos">Polica de datos</Link>.</p>
+							<div className="row">
+								<div className="col s12">
+									<Form onSubmit={this.onUsfxFormSubmit.bind(this)}>
+										<div>
+											<h3 style={{textAlign: 'center'}}>Acceso universitario</h3>
+										</div>
+										<div>
+											<TextInput name="cu" label="Carnet universitario" placeholder="CU" required={true}/>
+											<TextInput name="ci" label="Cédula de identidad" placeholder="CI" type="password" required={true}/>
+										</div>
+										<div>
+											<button className="kep-login-usfx" type="submit">
+												<span>Iniciar sesión</span>
+											</button>
+										</div>
+										<div>
+											<p className="note">Introduce tu numero de carnet universitario ej. (35-666), y tu numero de carnet de identidad ej. (4374391)</p>
+										</div>
+										<div>
+											<button className="" type="button" onClick={this.onBackToSignIn.bind(this)}>
+												<span>Volver</span>
+											</button>
+										</div>
+									</Form>
+								</div>
 							</div>
-
-							<div>
-								<button className="kep-login-usfx" type="button">
-									<span>Acceso universitario</span>
-								</button>
-								<FacebookLogin
-									appId="136635746918254"
-									fields="name,email,picture"
-									textButton="Iniciar con facebook"
-									icon="fa-facebook"
-									callback={this.onSignInWithFacebook.bind(this)} />
-							</div>
-						</Form>
+						</div>
 					</div>
 				</main>
 			</div>
