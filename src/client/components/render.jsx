dad56967230058429fee893d020b362c717f3490
Juan Carlos Labrandero <charly@localhost.localdomain>
2017-10-17 17:44:41 -0400

    Mouse trackball controls

--- src/client/components/render.jsx
+++ src/client/components/render.jsx
@@ -9,12 +9,14 @@
 import React from 'react';
 
 var THREE = require('three');
+var OIMO = require('oimo');
+var TrackballControls = require('three-trackballcontrols');
+
 
 /****************************************************************************************/
 
 class World {
 	constructor(props) {
-
 	}
 
 	init(params) {
@@ -28,19 +30,31 @@ class World {
 		this.clock = new THREE.Clock();
 
 		this.scene = new THREE.Scene();
-		this.camera = new THREE.PerspectiveCamera(75, view.width / view.height, 1, 10000);
+		this.camera = new THREE.PerspectiveCamera(45, view.width / view.height, 1, 10000);
 		this.camera.position.z = 500;
 		this.camera.position.y = 100;
+
+		this.trackball = new TrackballControls(this.camera, params.element);
+
+
 
 
 
 		var sphere = new THREE.SphereGeometry(10, 8, 8);
-		this.light1 = new THREE.PointLight(0xff0040, 1, 500);
+		this.light1 = new THREE.PointLight(0xff0040, 1, 1000);
 		this.light1.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xff0040 })));
 
 		this.scene.add(this.light1);
 		this.light1.position.y = 200;
 
+		var groundGeo = new THREE.PlaneBufferGeometry(5000, 1000);
+		var groundMat = new THREE.MeshPhongMaterial({color: 0xffffff, specular: 0x050505});
+		groundMat.color.setHSL(0.095, 1, 0.75);
+		var ground = new THREE.Mesh(groundGeo, groundMat);
+		ground.rotation.x = -Math.PI/2;
+		ground.position.y = -50;
+		this.scene.add(ground);
+		//ground.receiveShadow = true;
 
 		var boxGeo = new THREE.BoxGeometry(100, 100, 100);
 		var boxMat = new THREE.MeshPhongMaterial({color: 0xffffff});
@@ -51,19 +65,12 @@ class World {
 
 
 
+
+
 		this.renderer = new THREE.WebGLRenderer();
-		//this.renderer.setSize(view.width, view.height);
-
-
-		/*var para = document.createElement('P');
-		var t = document.createTextNode('This is a paragraph.');
-		para.appendChild(t);
-		this.renderer.domElement.appendChild(para);*/
-
 		this.renderer.domElement.classList.add('render-view');
 
 		params.element.appendChild(this.renderer.domElement);
-
 		this.render();
 	}
 
@@ -82,6 +89,10 @@ class World {
 		this.light1.position.z = Math.cos( time ) * 200;
 
 		//this.camera.aspect = view.width/view.height;
+
+		if(this.trackball) {
+			this.trackball.update();
+		}
 
 		this.renderer.setSize(view.width, view.height);
 		this.renderer.render(this.scene, this.camera);
